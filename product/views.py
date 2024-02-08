00f1641883c2f7d3bd1b589922871a8941fa40af
Yahya Ahmed <Yahya.Ahmed@DESKTOP>
2024-02-08 21:43:15 +0200

    added files

--- product/views.py
+++ product/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render
 from .models import Product
 from django.http import HttpResponse
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator ,EmptyPage, PageNotAnInteger
 
 # Create your views here.   
 
 def product_list(request):
     product_list=Product.objects.all()
     # paganator
-    paginator = Paginator(product_list, 3)  # Show 25 contacts per page.
+    paginator = Paginator(product_list, 10)  # Show 25 contacts per page.
     page= request.GET.get("page")
     product_list = paginator.get_page(page)
     
