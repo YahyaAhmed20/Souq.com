13c61e327fa9c6d77d59de8c2dd90c4acbb7a9d4
Yahya Ahmed <Yahya.Ahmed@DESKTOP>
2024-02-17 05:47:42 +0200

    Added new App called accounts and inside him added registration folder and inside him put my html pages

--- project/settings.py
+++ project/settings.py
@@ -33,16 +33,17 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     # 'product.apps.ProductConfig',
+    'accounts',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
+    # apps
     'product',
     'settings',    
-   
+    
     
 ]
 
@@ -137,7 +138,13 @@ MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,"media")
 
 
+
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL =  '/products'
+LOGOUT_REDIRECT_URL = '/products'
--- project/urls.py
+++ project/urls.py
@@ -22,6 +22,9 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include("product.urls",namespace="products")),
+    path("accounts/", include("django.contrib.auth.urls")),
+
+    
 ]
 urlpatterns+= static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
